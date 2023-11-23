67c109a01ca29eedf5d50afe75d7055cdd547b41
Hector Isaac Game Suarez <hectorisaacg.s@hotmail.com>
2023-11-23 20:12:28 +0000

    Si

--- test_game.py
+++ test_game.py
@@ -2,7 +2,7 @@ from tito_engine import *
 from custom_objs import *
 
 player = TopDownCharacterController(
-    3.0, path_to_img='assets/sprites/survivor-idle_knife_0.png',
+    3.0, path_to_img='assets/sprites/survivor-idle_handgun_0.png',
     transform=Transform(Vec2(540, 360), 0.0, 0.5),
     tag='Player'
 )
